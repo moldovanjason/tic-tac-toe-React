cd44f681563572591809e299fd2f739a3ab27f8c
moldovanjason <moldovanjason@gmail.com>
2020-07-30 21:55:52 +0000

    second

---
+++ src/js/view/home.js
@@ -0,0 +1,99 @@
+import React from "react";
+
+//create your first component
+export class Home extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			player: null,
+			square: null,
+			nextUserInput: "X",
+			cell1: "",
+			cell2: "",
+			cell3: "",
+			cell4: "",
+			cell5: "",
+			cell6: "",
+			cell7: "",
+			cell8: "",
+			cell9: ""
+		};
+	}
+
+	handleClick = cellId => {
+		if (cellId === 1) {
+			if (this.state.cell1 === "") {
+				this.setState({ cell1: this.state.nextUserInput });
+				this.setState({
+					nextUserInput: this.state.nextUserInput === "X" ? "O" : "X"
+				});
+			}
+		}
+		if (cellId === 2) {
+			if (this.state.cell2 === "") {
+				this.setState({ cell2: this.state.nextUserInput });
+				this.setState({
+					nextUserInput: this.state.nextUserInput === "X" ? "O" : "X"
+				});
+			}
+		}
+	};
+
+	render() {
+		return (
+			<div className="text-center mt-5">
+				<div className="game-row1">
+					<div
+						className="cell"
+						onClick={() => this.handleClick("cell1")}>
+						{this.state.cell1}
+					</div>
+					<div
+						className="cell"
+						onClick={() => this.handleClick("cell2")}>
+						{this.state.cell2}
+					</div>
+					<div
+						className="cell"
+						onClick={() => this.handleClick("cell3")}>
+						{this.state.cell3}
+					</div>
+				</div>
+				<div className="game-row1">
+					<div
+						className="cell"
+						onClick={() => this.handleClick("cell4")}>
+						{this.state.cell4}
+					</div>
+					<div
+						className="cell"
+						onClick={() => this.handleClick("cell5")}>
+						{this.state.cell5}
+					</div>
+					<div
+						className="cell"
+						onClick={() => this.handleClick("cell6")}>
+						{this.state.cell6}
+					</div>
+				</div>
+				<div className="game-row1">
+					<div
+						className="cell"
+						onClick={() => this.handleClick("cell7")}>
+						{this.state.cell7}
+					</div>
+					<div
+						className="cell"
+						onClick={() => this.handleClick("cell8")}>
+						{this.state.cell8}
+					</div>
+					<div
+						className="cell"
+						onClick={() => this.handleClick("cell9")}>
+						{this.state.cell9}
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
