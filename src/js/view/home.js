896f91cc5a4befe2b15578ea5c3db67d82300162
moldovanjason <moldovanjason@gmail.com>
2020-07-31 14:56:05 +0000

    third

--- src/js/view/home.js
+++ src/js/view/home.js
@@ -8,80 +8,67 @@ export class Home extends React.Component {
 			player: null,
 			square: null,
 			nextUserInput: "X",
-			cell1: "",
-			cell2: "",
-			cell3: "",
-			cell4: "",
-			cell5: "",
-			cell6: "",
-			cell7: "",
-			cell8: "",
-			cell9: ""
+			allCells: ["", "", "", "", "", "", "", "", ""],
+			winning: [
+				[1, 1, 1, 0, 0, 0, 0, 0, 0],
+				[0, 0, 0, 1, 1, 1, 0, 0, 0],
+				[0, 0, 0, 0, 0, 0, 1, 1, 1],
+				[1, 0, 0, 1, 0, 0, 1, 0, 0],
+				[0, 1, 0, 0, 1, 0, 0, 1, 0],
+				[0, 0, 1, 0, 0, 1, 0, 0, 1],
+				[1, 0, 0, 0, 1, 0, 0, 0, 1],
+				[0, 0, 1, 0, 1, 0, 1, 0, 0]
+			]
 		};
 	}
 	handleClick = cellId => {
-		let obj = {};
-		obj[cellId] = this.state.nextUserInput;
-		if (this.state[cellId] === "") {
-			this.setState(obj);
-			this.setState({
-				nextUserInput: this.state.nextUserInput === "X" ? "O" : "X"
-			});
-		}
+		var currentCells = this.state.allCells.map(
+			(element, i) => (cellId === i ? this.state.nextUserInput : element)
+		);
+		// currentCells is identifying the cell that is clicked. compare each index to the cell clicked and assigned new value from the nextUserInput
+
+		this.setState({ allCells: currentCells });
+		this.setState({
+			nextUserInput: this.state.nextUserInput === "X" ? "O" : "X"
+		});
+
+		// winnterCheck(currentCells);
 	};
 
 	render() {
 		return (
-			<div className="text-center mt-5">
+			<div className="page text-center mt-5">
 				<div className="game-row1">
-					<div
-						className="cell"
-						onClick={() => this.handleClick("cell1")}>
-						{this.state.cell1}
+					<div className="cell" onClick={() => this.handleClick(0)}>
+						{this.state.allCells[0]}
 					</div>
-					<div
-						className="cell"
-						onClick={() => this.handleClick("cell2")}>
-						{this.state.cell2}
+					<div className="cell" onClick={() => this.handleClick(1)}>
+						{this.state.allCells[1]}
 					</div>
-					<div
-						className="cell"
-						onClick={() => this.handleClick("cell3")}>
-						{this.state.cell3}
+					<div className="cell" onClick={() => this.handleClick(2)}>
+						{this.state.allCells[2]}
 					</div>
 				</div>
 				<div className="game-row1">
-					<div
-						className="cell"
-						onClick={() => this.handleClick("cell4")}>
-						{this.state.cell4}
+					<div className="cell" onClick={() => this.handleClick(3)}>
+						{this.state.allCells[3]}
 					</div>
-					<div
-						className="cell"
-						onClick={() => this.handleClick("cell5")}>
-						{this.state.cell5}
+					<div className="cell" onClick={() => this.handleClick(4)}>
+						{this.state.allCells[4]}
 					</div>
-					<div
-						className="cell"
-						onClick={() => this.handleClick("cell6")}>
-						{this.state.cell6}
+					<div className="cell" onClick={() => this.handleClick(5)}>
+						{this.state.allCells[5]}
 					</div>
 				</div>
 				<div className="game-row1">
-					<div
-						className="cell"
-						onClick={() => this.handleClick("cell7")}>
-						{this.state.cell7}
+					<div className="cell" onClick={() => this.handleClick(6)}>
+						{this.state.allCells[6]}
 					</div>
-					<div
-						className="cell"
-						onClick={() => this.handleClick("cell8")}>
-						{this.state.cell8}
+					<div className="cell" onClick={() => this.handleClick(7)}>
+						{this.state.allCells[7]}
 					</div>
-					<div
-						className="cell"
-						onClick={() => this.handleClick("cell9")}>
-						{this.state.cell9}
+					<div className="cell" onClick={() => this.handleClick(8)}>
+						{this.state.allCells[8]}
 					</div>
 				</div>
 			</div>
